4cf35b7dc526050b6ad61d75a91b638a3116a9f2
novendra <novendraar@gmail.com>
2021-10-14 18:14:25 +0700

    test dashboard

--- app/Views/admin/dashboard/dashboard.php
+++ app/Views/admin/dashboard/dashboard.php
@@ -71,7 +71,7 @@
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Jumlah Komentar</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $jmlkomentar ?></div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?php $jmlkomentar ?></div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fa fa-comments fa-2x text-gray-300"></i>
