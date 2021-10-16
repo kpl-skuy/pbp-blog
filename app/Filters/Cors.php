60c0605c1bd9280e83095e8b51bc001e7f741d84
mrapradanaundip <mrapradana@students.undip.ac.id>
2021-10-16 23:38:56 +0700

    add if

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -13,9 +13,12 @@ class Cors implements FilterInterface
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-        $method = $_SERVER['REQUEST_METHOD'];
-        if ($method == "OPTIONS") {
+        if (isset($_SERVER['REQUEST_METHOD']){
+            $method = $_SERVER['REQUEST_METHOD'];
+            if ($method == "OPTIONS") {
+            }
         }
+        
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
